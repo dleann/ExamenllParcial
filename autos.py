f63ea63a002e489ef52e851eb5032cb52eae8341
dleann <dinaleanng@gmail.com>
2020-03-13 17:13:34 -0600

    Agregando datos del auto

---
+++ autos.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import datetime
+
+class Autos:
+    def __init__ (self,placa="",marca="",modelo="",tipo="",hora_ingreso="",estado=""):
+        self.placa = placa
+        self.marca = marca
+        self.modelo = modelo
+        self.tipo=tipo
+        self.hora_ingreso = hora_ingreso
+        self.estado= True
